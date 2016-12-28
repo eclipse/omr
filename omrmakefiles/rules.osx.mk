db68e3269afafe0730f3c104a441f7cc02d08eba
Gavin <zheddie@163.com>
2016-12-28 15:42:37 +0800

    Make omr fit for customized GLUE.
    These changes are required for a cusotmized GLUE , instead of "example/glue".  I have give description in the issue #639. Please refer to it for details.
    
    Issue: #639
    Signed-off-by: Gavin Zhang zheddie@163.com

--- omrmakefiles/rules.osx.mk
+++ omrmakefiles/rules.osx.mk
@@ -28,7 +28,7 @@ GLOBAL_CXXFLAGS+=-fno-exceptions -fno-rtti -fno-threadsafe-statics
 ## Position Independent compile flag
 GLOBAL_CFLAGS+=-fPIC
 GLOBAL_CXXFLAGS+=-fPIC
-GLOBAL_LDFLAGS+=-fpic
+GLOBAL_LDFLAGS+=-fpic  -undefined dynamic_lookup
 
 GLOBAL_CFLAGS+= -m64
 GLOBAL_CXXFLAGS+=-m64
