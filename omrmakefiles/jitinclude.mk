e6433f1f59f7a94464c6ae0d9b600596cb2ad6a1
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2017-09-07 14:40:38 -0400

    Relicense under EPL v3 & Apache v2
    Update the license as per the outcome of the vote in issue #1596.
    
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- omrmakefiles/jitinclude.mk
+++ omrmakefiles/jitinclude.mk
@@ -1,19 +1,23 @@
 ###############################################################################
+# Copyright (c) 2015, 2015 IBM Corp. and others
 #
-# (c) Copyright IBM Corp. 2015
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which accompanies this
+# distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+# or the Apache License, Version 2.0 which accompanies this distribution and
+# is available at https://www.apache.org/licenses/LICENSE-2.0.
 #
-#  This program and the accompanying materials are made available
-#  under the terms of the Eclipse Public License v1.0 and
-#  Apache License v2.0 which accompanies this distribution.
+# This Source Code is also Distributed under one or more Secondary Licenses,
+# as those terms are defined by the Eclipse Public License, v. 2.0: GNU
+# General Public License, version 2 with the GNU Classpath Exception [1]
+# and GNU General Public License, version 2 with the OpenJDK Assembly
+# Exception [2].
 #
-#      The Eclipse Public License is available at
-#      http://www.eclipse.org/legal/epl-v10.html
-#
-#      The Apache License v2.0 is available at
-#      http://www.opensource.org/licenses/apache2.0.php
+# [1] https://www.gnu.org/software/classpath/license.html
+# [2] http://openjdk.java.net/legal/assembly-exception.html
 #
 # Contributors:
-#    Multiple authors (IBM Corp.) - initial implementation and documentation
+#   Multiple authors (IBM Corp.) - initial implementation and documentation
 ###############################################################################
 
 OMR_INCLUDES_FOR_JIT = $(OMR_DIR)/include_core $(OMR_DIR)/thread $(OMR_DIR)/gc/include
