faadc5d850d417b0e5cf8141741e35fa4cba0116
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2017-09-06 22:12:10 -0400

    Remove duplicate copyright
    Copyright was included in the file twice and due to
    the inane rules for the zOS assembler around line
    continuations based on characters in column 72 I've
    opted to just remove the duplicate copy.
    
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- port/zos390/omrvmem_support_below_bar_64.s
+++ port/zos390/omrvmem_support_below_bar_64.s
@@ -204,30 +204,6 @@ OMRSTORAGE RMODE ANY                                                     000000
 @@CCN@19 ALIAS C'omrfree_memory_below_bar'                               000000
 @@CCN@11 ALIAS C'omrallocate_4K_pages_below_bar'                         000000
 @@CCN@1  ALIAS C'omrallocate_1M_pageable_pages_below_bar'                000000
-************************************************************************
-* Copyright (c) 2001, 2016 IBM Corp. and others
-*
-* This program and the accompanying materials are made available under
-* the terms of the Eclipse Public License 2.0 which accompanies this
-* distribution and is available at
-* https://www.eclipse.org/legal/epl-1.0/ or the Apache License,
-* Version 2.0 which accompanies this distribution and is available at
-* https://www.apache.org/licenses/LICENSE-2.0.
-*
-* This Source Code is also Distributed under one or more Secondary
-* Licenses, as those terms are defined by the Eclipse Public License,
-*  v. 2.0: GNU General Public License, version 2 with the GNU
-* Classpath Exception [1] and GNU General Public License, version 2
-* with the OpenJDK Assembly Exception [2].
-*
-* [1] https://www.gnu.org/software/classpath/license.html
-* [2] http://openjdk.java.net/legal/assembly-exception.html
-*
-* Contributors:
-*   Multiple authors (IBM Corp.) - initial API and implementation
-*   and/or initial documentation
-*
-***********************************************************************
 * /*                                                                     000019
 *  * This file is used to generate the HLASM corresponding to the C cal  000020
 *  * that use the STORAGE macro in omrvmem.c                             000021
