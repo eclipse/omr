19c46c7da1c7873b14ba99218432c75372a29fd7
sam0208 <samvanderwoude@outlook.com>
2018-04-14 16:06:32 +0200

    Issue #2452

--- compiler/arm/codegen/OMRRegister.hpp
+++ compiler/arm/codegen/OMRRegister.hpp
@@ -64,24 +64,7 @@ class OMR_EXTENSIBLE Register: public OMR::Register
    uint64_t setInterference(uint64_t i) {return (_liveRegisterInfo._interference = i);}
 
 
-   /*
-    * Method for manipulating flags
-    */
-   bool isFlippedCCR()  {return _flags.testAny(FlipBranchOffThisCCR);}
-   void setFlippedCCR() {_flags.set(FlipBranchOffThisCCR);}
-   void resetFlippedCCR() {_flags.reset(FlipBranchOffThisCCR);}
-
-
    private:
-
-/* @@@@ copied from p/codegen/register/OMRRegister.hpp @@@@ */
-/* May need to be modified for ARM */
-
-   enum
-      {
-      FlipBranchOffThisCCR          = 0x4000, // PPC may have swapped register positions in compare
-      };
-
    // Both x and z also have this union but ppc uses uint64_t instead of uint32_t
    union
       {
