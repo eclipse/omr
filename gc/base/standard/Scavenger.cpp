ba76f36f2be455f335f87df8cfd48c13ec3b39a5
Salman Rana <salman.rana@ibm.com>
2018-10-17 11:12:39 -0400

    Experimental Build For Aliasing Threshold Investigation

--- gc/base/standard/Scavenger.cpp
+++ gc/base/standard/Scavenger.cpp
@@ -26,8 +26,9 @@
 #define OMR_SCAVENGER_TRACE_REMEMBERED_SET
 #define OMR_SCAVENGER_TRACE_BACKOUT
 #define OMR_SCAVENGER_TRACE_COPY
-#define OMR_SCAVENGER_TRACK_COPY_DISTANCE
 #endif
+
+#define OMR_SCAVENGER_TRACK_COPY_DISTANCE
 
 #include <math.h>
 
@@ -3117,7 +3118,9 @@ MM_Scavenger::aliasToCopyCache(MM_EnvironmentStandard *env, GC_SlotObject *scann
 	 *
 	 * @NOTE this is likely too aggressive and should be relaxed.
 	 */
-	if (0 == _waitingCount) {
+
+	if (_waitingCount <=  _extensions->waitCountThreshold) {
+
 		/* Only alias if the scanCache != copyCache. IF the caches are the same there is no benefit
 		 * to aliasing. The checks afterwards will ensure that a very similar copy order will happen
 		 * if the copyCache changes from the currently aliased scan cache
