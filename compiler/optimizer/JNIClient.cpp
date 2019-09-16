f885776a7261f65fe1b13e1318d40919e4222b2f
xiacijie <cijie@ualberta.ca>
2019-09-16 00:16:59 -0600

    Create JNI Client.
    This is a helper class that provides a high-level abstraction of Java Native Interface

--- compiler/optimizer/JNIClient.cpp
+++ compiler/optimizer/JNIClient.cpp
@@ -5,33 +5,286 @@
 #include "JNIClient.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <string.h>
 
-TR_JNIClient* TR_JNIClient::instance = NULL;
-JNIEnv * TR_JNIClient::env = NULL;
+/* ================= private ================= */
+TR_JNIClient* TR_JNIClient::_instance = NULL;
+JNIEnv * TR_JNIClient::_env = NULL;
 
 TR_JNIClient::TR_JNIClient() {
     //start jvm
     char *walaHome = std::getenv("WALA_HOME");
     char classpath[1024];
     sprintf(classpath, "%s/com.ibm.wala.util/target/classes:%s/com.ibm.wala.shrike/target/classes:%s/com.ibm.wala.core/target/classes:%s/com.ibm.wala.cast/target/classes", walaHome, walaHome, walaHome, walaHome);
-    env = launch_jvm(classpath);
+    _env = launch_jvm(classpath);
     std::cout << "Successfully launch JVM!" << std::endl;
 }
 
-TR_JNIClient* TR_JNIClient::getInstance() {
-    if (!instance){
-        instance = new TR_JNIClient;
-    }
-    return instance;
-}
 
-jclass TR_JNIClient::getClass(char *className) {
-    jclass cls = env->FindClass(className);
-    if (env->ExceptionCheck()){
+
+jclass TR_JNIClient::_getClass(const char *className) {
+    jclass cls = _env->FindClass(className);
+    if (_env->ExceptionCheck()){
         std::cout<< "Fail to find class "<< className << std::endl;
         exit(1);
     }
     return cls;
+}
+
+jmethodID TR_JNIClient::_getMethodID(bool isStaticMethod, jclass cls, const char *methodName, const char *methodSig) {
+    jmethodID mid;
+    if (isStaticMethod){
+        mid = _env->GetStaticMethodID(cls,methodName,methodSig);
+    }
+    else{
+        mid = _env->GetMethodID(cls,methodName,methodSig);
+    }
+
+    if (_env->ExceptionCheck()){
+        std::cout<< "Fail to find method "<< methodName << std::endl;
+        exit(1);
+    }
+    return mid;
+}
+
+/* ================= public ======================= */
+TR_JNIClient* TR_JNIClient::getInstance() {
+    if (!_instance){
+        _instance = new TR_JNIClient;
+    }
+    return _instance;
+}
+
+/* static methods */
+
+//Void
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    _env->CallStaticVoidMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// Object
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, jobject *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticObjectMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// int
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, int *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticIntMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// long
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, long *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticLongMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// short
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, short *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticShortMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// float
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, float *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticFloatMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// double
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, double *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticDoubleMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// char
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, char *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticCharMethodV(cls,mid,args);
+    va_end(args);
+}
+
+// char* (string)
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, char **res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    jstring out = (jstring)(_env->CallStaticObjectMethodV(cls,mid,args));
+    va_end(args);
+    char temp[1024];
+    sprintf(temp,_env->GetStringUTFChars(out,0));
+    *res = temp;
+}
+
+// bool
+void TR_JNIClient::callStaticMethod(MethodConfig methodConfig, bool *res, int argNum, ...) {
+
+    va_list args;
+    va_start(args,argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(true, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallStaticBooleanMethodV(cls,mid,args);
+    va_end(args);
+}
+
+
+/* non-static methods */
+
+//void
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    _env->CallVoidMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//object
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, jobject* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallObjectMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//int
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, int* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallIntMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//long
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, long* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallLongMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//short
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, short* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallShortMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//float
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, float* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallFloatMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//double
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, double* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallDoubleMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//char
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, char* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallCharMethodV(obj,mid,args);
+    va_end(args);
+}
+
+//bool
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, bool* res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    *res = _env->CallBooleanMethodV(obj,mid,args);
+    va_end(args);
+}
+
+// char* (string)
+void TR_JNIClient::callMethod(MethodConfig methodConfig, jobject obj, char **res, int argNum, ...) {
+
+    va_list args;
+    va_start(args, argNum);
+    jclass cls = _getClass(methodConfig.className);
+    jmethodID mid = _getMethodID(false, cls, methodConfig.methodName, methodConfig.methodSig);
+    jstring out = (jstring)(_env->CallObjectMethodV(obj,mid,args));
+    va_end(args);
+    char temp[1024];
+    sprintf(temp,_env->GetStringUTFChars(out,0));
+    *res = temp;
+
 }
 
 TR_JNIClient::~TR_JNIClient() {}
--- compiler/optimizer/JNIClient.hpp
+++ compiler/optimizer/JNIClient.hpp
@@ -1,5 +1,6 @@
 //
 // created by cijie xia on 2019-09-10.
+// An high-level abstraction of Java Native Interface
 //
 
 #ifndef omr_jniclient_h
@@ -7,17 +8,49 @@
 
 #include "launch.h"
 
+struct MethodConfig {
+    char* className;
+    char* methodName;
+    char* methodSig;
+};
+
 // singleton pattern
 class TR_JNIClient
 {
 private:
     TR_JNIClient();
-    static JNIEnv *env;
-    static TR_JNIClient *instance;
+    static JNIEnv *_env;
+    static TR_JNIClient *_instance;
+    jclass _getClass(const char* className);
+    jmethodID _getMethodID(bool isStaticMethod, jclass cls, const char* methodName, const char* methodSig);
 
 public:
     ~TR_JNIClient();
     static TR_JNIClient *getInstance();
-    jclass getClass(char* className);
+
+    /*static*/
+    void callStaticMethod(MethodConfig methodConfig,              int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, jobject* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, int* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, long* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, short* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, float* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, double* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, char* res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, char** res, int argNum, ...);
+    void callStaticMethod(MethodConfig methodConfig, bool* res, int argNum, ...);
+
+    /* non-static */
+    void callMethod(MethodConfig methodConfig, jobject obj,              int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, jobject* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, int* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, long* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, short* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, float* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, double* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, char* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, bool* res, int argNum, ...);
+    void callMethod(MethodConfig methodConfig, jobject obj, char** res, int argNum, ...);
+
 };
 #endif //omr_jniclient_h
