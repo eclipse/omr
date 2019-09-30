1cc3834e7a8e00c580943baa6f7567baea3dd0b5
xiacijie <cijie@ualberta.ca>
2019-09-30 01:18:59 -0600

    Map 'goto' instruction into WALA 'goto' instruction and refactor OWLMapper class.
    Extract WALA instruction constructors from OWLMapper class and create a new class OWLInstructionConstructor. This class will construct the WALA instructions and come with some helper methods necessary in constructing WALA instructions.

--- compiler/optimizer/OWLJNIClient.hpp
+++ compiler/optimizer/OWLJNIClient.hpp
@@ -9,6 +9,7 @@
 #include "launch.h"
 
 struct MethodConfig {
+    bool isStatic;
     char* className;
     char* methodName;
     char* methodSig;
@@ -37,25 +38,15 @@ public:
     static TR_OWLJNIClient *getInstance();
 
     jstring constructString(char* str);
-    jobject constructIntObject(int i);
+    jobject constructIntegerObject(int i);
+    jobject constructFloatObject(float i);
+    jobject constructDoubleObject(double i);
+    jobject constructShortObject(short i);
+    jobject constructLongObject(long i);
 
     /* Field */
     void getField(FieldConfig fieldConfig, jobject obj, jobject*res);
 
-    /*static*/
-    void callStaticMethod(MethodConfig methodConfig,              int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, jobject* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, int* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, long* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, short* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, float* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, double* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, char* res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, char** res, int argNum, ...);
-    void callStaticMethod(MethodConfig methodConfig, bool* res, int argNum, ...);
-
-    /* non-static */
-    void callMethod(MethodConfig methodConfig, jobject obj,              int argNum, ...);
     void callMethod(MethodConfig methodConfig, jobject obj, jobject* res, int argNum, ...);
     void callMethod(MethodConfig methodConfig, jobject obj, int* res, int argNum, ...);
     void callMethod(MethodConfig methodConfig, jobject obj, long* res, int argNum, ...);
