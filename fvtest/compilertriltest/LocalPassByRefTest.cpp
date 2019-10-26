7db6a92ae3d0a2e86bd872a47f938693274b77dc
Dibyendu Majumdar <mobile@majumdar.org.uk>
2019-10-26 01:34:03 +0100

    Reformat the tril test code to make it more readable
    Also added a comment to explain the purpose of the test.
    
    Signed-off-by: Dibyendu Majumdar <mobile@majumdar.org.uk>

--- fvtest/compilertriltest/LocalPassByRefTest.cpp
+++ fvtest/compilertriltest/LocalPassByRefTest.cpp
@@ -24,6 +24,26 @@
 
 class LocalPassByRefTest : public TRTest::JitTest {};
 
+/*
+ * Purpose of this test is to exercise the
+ * optimizer when local stack variables (autos) are
+ * passed by reference to functions, which may modify the
+ * values. Thus the functin aliases the local stack values
+ * and the optimizer needs to be aware of this aliasing.
+ *
+ * This requires two steps:
+ *
+ * The optimizer flag callsAsUses must be true. This flag
+ * controls whether the optimizer will consider functions as
+ * possibly aliasing local stack values.
+ *
+ * Secondly the language front end needs to set the aliasing
+ * data whenever such aliasing can occur.
+ *
+ * The test below does neither of these things (as of now),
+ * hence will fail.
+ */
+
 int foo(int*i, long long*l)
 {
 	*i = (int) *l;
@@ -31,22 +51,41 @@ int foo(int*i, long long*l)
 }
 
 TEST_F(LocalPassByRefTest, passByRefTest) { 
-    char inputTrees[512] = {0};
-	const auto format_string = "(method return=Int32 (block (istore temp=\"a\" (iconst 5)) (lstore temp=\"b\" (lconst 42)) (treetop (icall address=0x%jX args=[Address, Address] (loadaddr temp=\"a\") (loadaddr temp=\"b\") )) (ireturn (iadd (l2i (lload temp=\"b\")) (iload temp=\"a\")))))";
+    char inputTrees[1024] = {0};
+	const char format_string[] = 
+		"(method return=Int32          "
+		"  (block                      "
+		"    (istore temp=\"a\"        "
+		"      (iconst 5)              "
+		"    )                         "
+		"    (lstore temp=\"b\"        "
+		"      (lconst 42)             "
+		"    )                         "
+		"    (treetop                  "
+		"      (icall address=0x%jX args=[Address, Address] "
+		"        (loadaddr temp=\"a\") "
+		"        (loadaddr temp=\"b\") "
+		"      )                       "
+		"    )                         "
+		"    (ireturn                  "
+		"      (iadd                   "
+		"        (l2i                  "
+		"          (lload temp=\"b\")  "
+		"        )                     "
+		"        (iload temp=\"a\")    "
+		"      )                       "
+		"    )                         "
+		"  )                           "
+		")";
     std::snprintf(inputTrees, sizeof inputTrees, format_string, reinterpret_cast<uintmax_t>(&foo));
     auto trees = parseString(inputTrees);
 
     ASSERT_NOTNULL(trees) << "Trees failed to parse\n" << inputTrees;
 
-    // Execution of this test is disabled on non-X86 platforms, as we 
-    // do not have trampoline support, and so this call may be out of 
-    // range for some architectures. 
-#ifdef TR_TARGET_X86
     Tril::DefaultCompiler compiler{trees};
     ASSERT_EQ(0, compiler.compile()) << "Compilation failed unexpectedly\n" << "Input trees: " << inputTrees;
 
     auto entry_point = compiler.getEntryPoint<int32_t (*)()>();
 
     EXPECT_EQ(84, entry_point());
-#endif
 }
