74c89f9aba6c6cd88dcf75c936233e49684050b3
Salman Rana <salman.rana@ibm.com>
2019-10-31 10:44:23 -0400

    Force CS test osX

--- gc/base/GCExtensionsBase.hpp
+++ gc/base/GCExtensionsBase.hpp
@@ -1509,7 +1509,7 @@ public:
 #if defined(OMR_GC_CONCURRENT_SCAVENGER)
 		, softwareRangeCheckReadBarrier(false)
 		, concurrentScavenger(false)
-		, concurrentScavengerForced(false)
+		, concurrentScavengerForced(true)
 		, concurrentScavengerHWSupport(false)
 		, concurrentScavengerBackgroundThreads(1)
 		, concurrentScavengerBackgroundThreadsForced(false)
