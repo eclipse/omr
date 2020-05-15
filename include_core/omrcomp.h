64a548b510bb22a8e58d7b4a4ff0d6f0bc4cdbcd
Devin Nakamura <devinn@ca.ibm.com>
2020-05-15 00:50:06 -0400

    Add Inline support for c on zos if available
    Signed-off-by: Devin Nakamura <devinn@ca.ibm.com>

--- include_core/omrcomp.h
+++ include_core/omrcomp.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2019 IBM Corp. and others
+ * Copyright (c) 1991, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -333,6 +333,10 @@ typedef struct {
 #define VMINLINE_ALWAYS inline
 #endif
 
+#else /* __cplusplus */
+#if defined(__C99_INLINE)
+#define VM_INLINE_ALWAYS inline
+#endif
 #endif /* __cplusplus */
 
 #else
