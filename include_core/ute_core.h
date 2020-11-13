fe7ef6cb3ecb5db57580cf7b3806ce25612352aa
Alex Xu (Hello71) <alex_y_xu@yahoo.ca>
2020-11-13 17:35:55 -0500

    WIP: try zero-sized array for AIX

--- include_core/ute_core.h
+++ include_core/ute_core.h
@@ -125,7 +125,7 @@ typedef struct UtTraceRecord {
 	uint64_t threadSyn2;	/* Thread synonym 2               */
 	int32_t firstEntry;		/* Offset to first trace entry    */
 	int32_t nextEntry;		/* Offset to next entry           */
-	char threadName[];		/* Thread name                    */
+	char threadName[0];		/* Thread name                    */
 } UtTraceRecord;
 
 /*
