37578c2e657b5ae86d0061040ceb64546c1b0770
Kevin Dean <kadean08@gmail.com>
2022-10-19 13:24:31 -0400

    WIP: Replace placeholder DataTypes enum values
    The DataTypes enum contains some placeholders representing values
    such as the number of different categories of DataTypes. To prevent
    those placeholders from being treated as though they are a value
    representing a valid DataType, mark the first and last of each category
    of DataTypes instead and remove the placeholders from the enum.
    For those placeholder values used extensively elsewhere, replace them
    with a constant in the TR namespace of the same value.
    
    Introduce FirstTRScalarType and LastTRScalarType in DataTypesEnum.hpp
    to indicate to downstream projects that they should set these values
    appropriately based on their extensions to the DataTypes enum.
    
    Issue: #6709

--- compiler/il/DataTypesEnum.hpp
+++ compiler/il/DataTypesEnum.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,3 +20,11 @@
  *******************************************************************************/
 
 // This file is used by projects to define their own data types
+
+#ifndef DATATYPES_ENUM_INCL
+#define DATATYPES_ENUM_INCL
+
+FirstTRScalarType = FirstOMRScalarType,
+LastTRScalarType = LastOMRScalarType,
+
+#endif
